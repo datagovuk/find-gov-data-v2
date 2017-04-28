152767120640fe460f653ce039af2f63a70045a9
Max Froumentin <max@froumentin.net>
2017-04-28 10:47:41 +0100

    Add Team Confidence prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,53 @@ $(document).ready(function () {
   // to toggle hidden content
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
+
+  var showHide = new ShowHide()
+  showHide.init()
 })
+
+var ShowHide = function() {
+  this.selector = '.showHide'
+  this.controlSelector = '.showHide-control'
+  this.contentSelector = '.showHide-content'
+  this.openSelector = '.showHide-open-all'
+  this.allOpen = false;
+}
+
+ShowHide.prototype = {
+  toggle : function(event) {
+    var parentShowHide = $(event.target).parents(this.selector)
+    var isOpen = parentShowHide.data("isOpen")
+    parentShowHide.data("isOpen", !isOpen)
+    parentShowHide.find(this.contentSelector).toggle()
+    parentShowHide.find(this.controlSelector).html(isOpen? '+' : '-');
+  },
+  toggleAll : function(event) {
+    var showHideDataLinks = $(event.target).parents('.data-links')
+    event.preventDefault()
+    var allOpen = $(event.target).data("allOpen")
+    console.log(allOpen)
+    if (allOpen) {
+      showHideDataLinks.find(this.selector).data('isOpen', false)
+      showHideDataLinks.find(this.contentSelector).hide()
+      showHideDataLinks.find(this.controlSelector).html('+')
+      $(event.target).data("allOpen", false)
+      $(event.target).text('Open all')
+
+    } else {
+      showHideDataLinks.find(this.selector).data('isOpen', true)
+      showHideDataLinks.find(this.contentSelector).show()
+      showHideDataLinks.find(this.controlSelector).html('-')
+      $(event.target).data("allOpen", true)
+      $(event.target).text('Close all')
+    }
+
+  },
+
+  init : function() {
+    $(this.controlSelector).on('click', this.toggle.bind(this))
+    $(this.openSelector).on('click', this.toggleAll.bind(this))
+    $(this.selector).data("isOpen", false)
+    $(this.openSelector).data("allOpen", false)
+  }
+}
